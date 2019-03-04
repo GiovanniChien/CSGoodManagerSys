f5525151bbf09f1e1374ed497993317741a5a1bb
文超 张 <1239374273@qq.com>
2019-03-04 09:21:40 +0800

    update Login 3-4 by vinret

---
+++ GoodManagerSys/GoodManagerSys/Encrypt.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Security.Cryptography;
+
+public class Encrypt
+{
+    public static string GetMD5(string password)
+    {
+        MD5 md5 = new MD5CryptoServiceProvider();
+        byte[] fromData = System.Text.Encoding.Default.GetBytes(password);
+        byte[] targetData = md5.ComputeHash(fromData);
+        string byte2String = null;
+        for (int i = 0; i < targetData.Length; i++)
+        {
+            byte2String += targetData[i].ToString("X2");
+        }
+        return byte2String;
+    }
+}
