b384999b8e90921bee6756e6e46afc95edf4c14b
文超 张 <1239374273@qq.com>
2019-03-04 16:51:52 +0800

    update FrmWarehouse 3-4 by vinret

--- GoodManagerSys/GoodManagerSys/Frm/Warehouse/FrmWarehouse.Designer.cs
+++ GoodManagerSys/GoodManagerSys/Frm/Warehouse/FrmWarehouse.Designer.cs
@@ -28,44 +28,163 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.DgvWarehouse = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column8 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column10 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column7 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column9 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column11 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.BtnExit = new System.Windows.Forms.Button();
             this.TxtIdOrName = new System.Windows.Forms.TextBox();
             this.BtnQuery = new System.Windows.Forms.Button();
             this.CmbCategory = new System.Windows.Forms.ComboBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DgvWarehouse)).BeginInit();
             this.SuspendLayout();
             // 
-            // dataGridView1
+            // DgvWarehouse
             // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(36, 85);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.RowTemplate.Height = 27;
-            this.dataGridView1.Size = new System.Drawing.Size(728, 280);
-            this.dataGridView1.TabIndex = 0;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.Aqua;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.DgvWarehouse.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.DgvWarehouse.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DgvWarehouse.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column6,
+            this.Column4,
+            this.Column5,
+            this.Column8,
+            this.Column10,
+            this.Column7,
+            this.Column9,
+            this.Column11});
+            this.DgvWarehouse.Location = new System.Drawing.Point(37, 85);
+            this.DgvWarehouse.Name = "DgvWarehouse";
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.DgvWarehouse.RowHeadersDefaultCellStyle = dataGridViewCellStyle2;
+            this.DgvWarehouse.RowTemplate.Height = 20;
+            this.DgvWarehouse.Size = new System.Drawing.Size(728, 280);
+            this.DgvWarehouse.TabIndex = 0;
+            // 
+            // Column1
+            // 
+            this.Column1.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.Column1.HeaderText = "编号";
+            this.Column1.Name = "Column1";
+            this.Column1.Width = 66;
+            // 
+            // Column2
+            // 
+            this.Column2.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.Column2.HeaderText = "名称";
+            this.Column2.Name = "Column2";
+            this.Column2.Width = 66;
+            // 
+            // Column3
+            // 
+            this.Column3.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.Column3.HeaderText = "大类";
+            this.Column3.Name = "Column3";
+            this.Column3.Width = 66;
+            // 
+            // Column6
+            // 
+            this.Column6.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.Column6.HeaderText = "厂商";
+            this.Column6.Name = "Column6";
+            this.Column6.Width = 66;
+            // 
+            // Column4
+            // 
+            this.Column4.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.Column4.HeaderText = "单位";
+            this.Column4.Name = "Column4";
+            this.Column4.Width = 66;
+            // 
+            // Column5
+            // 
+            this.Column5.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.Column5.HeaderText = "颜色";
+            this.Column5.Name = "Column5";
+            this.Column5.Width = 66;
+            // 
+            // Column8
+            // 
+            this.Column8.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.Column8.HeaderText = "保质期";
+            this.Column8.Name = "Column8";
+            this.Column8.Width = 81;
+            // 
+            // Column10
+            // 
+            this.Column10.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.Column10.HeaderText = "当前库存";
+            this.Column10.Name = "Column10";
+            this.Column10.Width = 96;
+            // 
+            // Column7
+            // 
+            this.Column7.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.Column7.HeaderText = "最小库存";
+            this.Column7.Name = "Column7";
+            this.Column7.Width = 96;
+            // 
+            // Column9
+            // 
+            this.Column9.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.Column9.HeaderText = "最大库存";
+            this.Column9.Name = "Column9";
+            this.Column9.Width = 96;
+            // 
+            // Column11
+            // 
+            this.Column11.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.Column11.HeaderText = "有效状态";
+            this.Column11.Name = "Column11";
+            this.Column11.Width = 96;
             // 
             // BtnExit
             // 
-            this.BtnExit.Location = new System.Drawing.Point(626, 386);
+            this.BtnExit.Location = new System.Drawing.Point(637, 386);
             this.BtnExit.Name = "BtnExit";
             this.BtnExit.Size = new System.Drawing.Size(96, 33);
             this.BtnExit.TabIndex = 1;
             this.BtnExit.Text = "退出";
             this.BtnExit.UseVisualStyleBackColor = true;
+            this.BtnExit.Click += new System.EventHandler(this.BtnExit_Click);
             // 
             // TxtIdOrName
             // 
-            this.TxtIdOrName.Location = new System.Drawing.Point(431, 38);
+            this.TxtIdOrName.Location = new System.Drawing.Point(460, 38);
             this.TxtIdOrName.Name = "TxtIdOrName";
             this.TxtIdOrName.Size = new System.Drawing.Size(166, 25);
             this.TxtIdOrName.TabIndex = 3;
             // 
             // BtnQuery
             // 
-            this.BtnQuery.Location = new System.Drawing.Point(633, 36);
+            this.BtnQuery.Location = new System.Drawing.Point(644, 36);
             this.BtnQuery.Name = "BtnQuery";
             this.BtnQuery.Size = new System.Drawing.Size(89, 27);
             this.BtnQuery.TabIndex = 4;
@@ -98,17 +217,17 @@
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(85, 19);
             this.label1.TabIndex = 6;
-            this.label1.Text = "商品类别";
+            this.label1.Text = "商品大类";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("宋体", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.label2.Location = new System.Drawing.Point(283, 41);
+            this.label2.Location = new System.Drawing.Point(266, 41);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(142, 19);
+            this.label2.Size = new System.Drawing.Size(180, 19);
             this.label2.TabIndex = 7;
-            this.label2.Text = "商品编号或名称";
+            this.label2.Text = "商品类别编号或名称";
             // 
             // FrmWarehouse
             // 
@@ -121,11 +240,11 @@
             this.Controls.Add(this.BtnQuery);
             this.Controls.Add(this.TxtIdOrName);
             this.Controls.Add(this.BtnExit);
-            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.DgvWarehouse);
             this.Name = "FrmWarehouse";
             this.Text = "FrmWarehouse";
             this.Load += new System.EventHandler(this.FrmWarehouse_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DgvWarehouse)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -133,12 +252,23 @@
 
         #endregion
 
-        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView DgvWarehouse;
         private System.Windows.Forms.Button BtnExit;
         private System.Windows.Forms.TextBox TxtIdOrName;
         private System.Windows.Forms.Button BtnQuery;
         private System.Windows.Forms.ComboBox CmbCategory;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column6;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column8;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column10;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column7;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column9;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column11;
     }
 }
